cd0367a092d320694363c74ce8996ae209e869f4
Parker Catalano <catalano.work@gmail.com>
2017-11-07 21:58:04 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,14 @@
-# Your code goes here!
+require 'pry'
+
+class Anagram
+  attr_accessor :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match
+    @word.detect{|i| i == word}
+  end
+  
+end
